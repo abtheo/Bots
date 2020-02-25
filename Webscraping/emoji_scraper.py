78393e8b4e6a0575b750a23fa2d02f9b6b379e03
abtheo <c.theo.kent@gmail.com>
2020-02-25 03:26:10 +0100

    Added webscraper for emoji history

---
+++ Webscraping/emoji_scraper.py
@@ -0,0 +1,36 @@
+import requests 
+from bs4 import BeautifulSoup 
+import csv
+
+entries=[]  # a list to store quotes 
+  
+#Loop all history pages
+for n in range(1,88):
+    URL = f"https://freemojilottery.com/history/page/{n}"
+    r = requests.get(URL)   
+    soup = BeautifulSoup(r.content, 'html5lib') 
+
+    table = soup.find_all('tr') 
+    for i,row in enumerate(table):
+        if i == 0:
+            continue
+        #Filter date
+        date = row.find("td", {"class": "draw-date"}).span.get_text()
+
+        #Filter emoji winners
+        emojis = row.findAll("object", {"class": "emojione"})
+        row_emojis = str([emo['standby'].encode('unicode-escape') for emo in emojis])
+        
+        #Retain parsed row
+        entry = {}
+        entry['timestamp'] = date
+        entry['value'] = row_emojis
+        entries.append(entry)
+
+#Save to output file
+filename = 'emoji_history.csv'
+with open(filename, 'w') as f: 
+    w = csv.DictWriter(f,['timestamp','value']) 
+    w.writeheader()
+    for e in entries: 
+        w.writerow(e) 
